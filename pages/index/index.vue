652ef7432b7b8ceea43daf8c465f13bae78de2e2
jl <atal163@sina.com>
2019-07-17 12:10:33 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,84 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		<view class="uni-list">
+			<view class="uni-list-cell" hover-class="uni-list-cell-hover" v-for="(item,index) in news" :key="index" @tap="openinfo" :data-newsid="item.post_id">
+				<view class="uni-media-list">
+					<image class="uni-media-list-logo" :src="item.author_avatar"></image>
+					<view class="uni-media-list-body">
+						<view class="uni-media-list-text-top">{{item.title}}</view>
+						<view class="uni-media-list-text-bottom uni-ellipsis">{{item.created_at}}</view>
+					</view>
+				</view>
+			</view>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				news: []
+			}
+		},
+		onLoad() {
+			console.log('>>>111')
+			uni.request({
+				url: 'https://unidemo.dcloud.net.cn/api/news',
+				method: 'GET',
+				data: {},
+				success: res => {
+					console.log(res);
+					this.news = res.data;
+				},
+				fail: () => {},
+				complete: () => {}
+			});
+		},
+		methods: {
+			openinfo(e) {
+				var newsid = e.currentTarget.dataset.newsid;
+				console.log(newsid)
+				uni.navigateTo({
+					url: '../info/info?newsid='+newsid
+				});
+			}
+		},
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200upx;
+		width: 200upx;
+		margin-top: 200upx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50upx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+	.uni-media-list-body{height: auto;}
+	.uni-media-list-text-top{line-height: 1.6em;}
+</style>
